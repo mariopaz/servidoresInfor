0bfcdfbd84bcb8ee03ad1b019dc85d9b588368ba
mariopaz <mariopaz2998@gmail.com>
2021-06-13 08:16:02 -0600

    Se crea la clase productos

--- src/db/productos.php
+++ src/db/productos.php
@@ -8,38 +8,13 @@ class Productos
       $this->conexion = (new Conexion())->conectar();
       $this->redis = new Redis();
       $this->redis->connect('redis', 6379);
+
    }
    // Funcion obtener por categoria
 
-   function obtenerC($categoria){
-      
-      if(!$this->redis->get($categoria)) {
-         $sql  = "SELECT * FROM productos WHERE categoria = '".$categoria."' ";
-        
-         $consulta = $this->conexion->query($sql);
-         $datos = $consulta->fetch_all(MYSQLI_ASSOC);
-         $this->redis->set($categoria, serialize($datos));
-         $this->redis->expire($categoria, 120);
-         return $datos;
-      } else {
-         return unserialize($this->redis->get($categoria));
-      }
-}
+   
 
    // Funcion obtener destacados
 
-function obtenerDestacados() {
-      if(!$this->redis->get("Destacados")) {
-         $sql  = "SELECT * FROM productos group by categoria";
-        
-         $consulta = $this->conexion->query($sql);
-         $datos = $consulta->fetch_all(MYSQLI_ASSOC);
-         $this->redis->set("Destacados", serialize($datos));
-         $this->redis->expire("Destacados", 120);
-         return $datos;
-      } else {
-         return unserialize($this->redis->get("Destacados"));
-      }
-}
 
 }
